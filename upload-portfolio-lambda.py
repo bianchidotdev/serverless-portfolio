2374e7092c099d0b4d60930ad432817d0422469f
flynn2812 <michaeldbianchi@gmail.com>
2017-10-03 11:05:32 -0500

    Use SNS to notify when portfolio is deployed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,22 +6,30 @@ import boto3
 from botocore.client import Config
 
 def lambda_handler(event, context):
-
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-2:394994090208:deployPortfolioTopic')
     
-    portfolio_bucket = s3.Bucket('portfolio.blualism.com')
-    build_bucket = s3.Bucket('portfoliobuild.blualism.com')
-    
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    
-    print "Job done"
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        
+        portfolio_bucket = s3.Bucket('portfolio.blualism.com')
+        build_bucket = s3.Bucket('portfoliobuild.blualism.com')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        
+        print "Job done"
+        topic.publish(Subject='Portfolio Deployed', Message="Portfolio deployed successfully.")
+    except:
+        topic.publish(Subject='Portfolio Deployed Failed', Message="The portfolio was not deployed successfully.")
+        raise
     
     return "Hello from lambda"
